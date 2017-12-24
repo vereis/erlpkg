c200f636f39477f9a7066bf8c3cdc99631fd52be
vereis <me@cbailey.co.uk>
2017-12-24 11:01:40 +0000

    Added ability to embed different file types including directories and beam files into escript package - added bin for erlpkg

--- src/erlpkg.erl
+++ src/erlpkg.erl
@@ -22,6 +22,11 @@ main(Args) ->
 
 %% Takes a list of parameters, starting with a Package Name and then a list of files 
 %% to include in said package, and builds an escript package.
+%%
+%% Note : The given Pkg_name determines which module in an escript is assumed to be
+%%        the main entrypoint said escript. I.e. running erlpkg on itself with
+%%        'erlpkg erlpkg erlpkg.erl' will produce a working erlpkg escript whereas
+%%        trying to create an escript with 'erlpkg erlpkg_escript erlpkg.erl' will not.
 build([Pkg_name | Files_to_include]) ->
     io:format("Preparing to build package: ~p...~n", [Pkg_name]),
     
@@ -36,15 +41,50 @@ build([Pkg_name | Files_to_include]) ->
 
 %% Shorthand for building escript headers
 build_header() ->
-    <<"#!/usr/bin/env escript\n%%\n%%! -A0 +sbtu\n">>.
+    <<"#!/usr/bin/env escript\n%%! -A0 +sbtu\n">>.
 
-%% Compiles a given filename and returns its binary data so we can add it to an escript
-%% package.
+%% Processes a file and appends its data to an escript package. Determines how to process given file
+%% by reading file info to check whether or not input is a directory, and if it isn't, reading file extension.
 build_file(Filename) ->
-    io:format("==> Including file: ~s~n", [Filename]),
-    {ok, _, Compiled_file} = compile:file(Filename, [binary]),
+    {ok, File_info} = file:read_file_info(Filename),
+    case tuple_to_list(File_info) of
+        [file_info, _, directory | _] ->
+            build_file(dir, Filename);
+        [file_info, _, regular | _] ->
+            [$. | Type] = filename:extension(Filename),
+            build_file(list_to_atom(Type), Filename)
+    end.
+
+%% Compiles a given erlang source file and returns its binary data so we can add it to an escript
+%% package.
+build_file(erl, Filename) ->
+    io:format("==> Including Erlang source file: ~s~n", [Filename]),
+    io:format("~4cCompiling Erlang source file in memory....~n", [$ ]),
+    {ok, ModuleName, Compiled_file} = compile:file(Filename, [binary]),
     io:format("~4cok.~n", [$ ]),
-    {Filename, Compiled_file}.
+    {atom_to_list(ModuleName) ++ ".beam", Compiled_file};
+
+%% Zips up a directory and returns its binary data so we can add it to an escript package
+build_file(dir, Filename) ->
+    io:format("==> Including directory: ~s/~n", [Filename]),
+    io:format("~4cCompressing and zipping directory in memory....~n", [$ ]),
+    {ok, {_, Zipped_contents}} = zip:create(Filename ++ ".zip", [Filename], [memory, {compress, all}]),
+    io:format("~4cok.~n", [$ ]),
+    {Filename ++ ".zip", Zipped_contents};
+
+%% Reads a given beam file and attaches its binary data so we can add it to an escript package
+build_file(beam, Filename) ->
+    io:format("==> Including Erlang bytecode file: ~s~n", [Filename]),
+    {ok, File_data} = file:read_file(Filename),
+    io:format("~4cok.~n", [$ ]),
+    {Filename, File_data};
+
+%% Blindly reads a given file for its binary data so we can add it to an escript package
+build_file(_, Filename) ->
+    io:format("==> Including unknown type file: ~s~n", [Filename]),
+    {ok, File_data} = file:read_file(Filename),
+    io:format("~4cok.~n", [$ ]),
+    {Filename, File_data}.
 
 %% Displays help information
 usage_instructions() ->
